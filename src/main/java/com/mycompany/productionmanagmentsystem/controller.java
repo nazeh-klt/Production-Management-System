7c0068824391e9f7fcc6b11a6e419578942de499
Your Name <you@example.com>
2025-12-10 14:45:56 +0200

    add interface

---
+++ src/main/java/com/mycompany/productionmanagmentsystem/controller.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.productionmanagmentsystem;
+
+/**
+ *
+ * @author USER
+ */
+public interface controller {
+    public void add();
+    public void remove();
+    public void find_by_id();
+}
