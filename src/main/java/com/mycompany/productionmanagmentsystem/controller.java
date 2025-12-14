607ef992596e87b06bacf25d098e7fb3676304bb
The Hunter <nournoudy2006@gmail.com>
2025-12-14 15:57:53 +0300

    Changed parameters in the controller interface

--- src/main/java/com/mycompany/productionmanagmentsystem/controller.java
+++ src/main/java/com/mycompany/productionmanagmentsystem/controller.java
@@ -3,15 +3,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package com.mycompany.productionmanagmentsystem;
-
+import java.util.*;
 /**
  *
  * @author USER
  */
 public interface controller {
-    public void add();
-    public void remove();
-    public void load_to_file();
-    public void save_to_file();
-    public void find_by_id();
+    public void add(Object object);
+    public void remove(Object object);
+    public void load_to_file(Set set);
+    public void save_to_file(Set set);
+    public void find_by_id(int id);
 }
