4651a6a4e494f6a0cf60efe94278c566eef58411
The Hunter <nournoudy2006@gmail.com>
2025-12-21 21:37:07 +0300

    fixed code that was not updated

--- src/main/java/com/mycompany/productionmanagmentsystem/ItemController.java
+++ src/main/java/com/mycompany/productionmanagmentsystem/ItemController.java
@@ -11,9 +11,9 @@ import java.io.*;
  *
  * @author Admin
  */
-public class ItemController{
+public class ItemController {
 
-    Set<Item> Items;
+    static Set<Item> Items;
 
     public void add(Item item) {
         Items.add(item);
@@ -24,25 +24,27 @@ public class ItemController{
     }
 
     public void load_to_file(Set l_items) {
-        try(BufferedReader ItemReader = new BufferedReader(new FileReader("items.csv"));){
-            for(Item item: Items){
+        try (BufferedReader ItemReader = new BufferedReader(new FileReader("items.csv"));) {
+            String line;
+            while ((line = ItemReader.readLine())!= null) {
+                String[] attributes = line.split(",");
+                Item i = new Item(Integer.parseInt(attributes[0]),attributes[1],Integer.parseInt(attributes[2]),Integer.parseInt(attributes[3]),Integer.parseInt(attributes[4]));
             }
 
+
+        } catch (Exception e) {
+
         }
-        catch(Exception e){
-            
-        }
-            
-            
+
     }
 
     public void save_to_file(Set s_items) {
         try (BufferedWriter ItemWriter = new BufferedWriter(new FileWriter("items.csv"));) {
             ItemWriter.write("id, category, price, available_amount, least_allowed_amount");
-            for(Item item: Items)
-                ItemWriter.write('\n' + item.id + "" + item.category +"" +item.price +"" +"" + item.available_amount +"" + item.least_allowed_amount);
-        } 
-        catch (Exception e) {
+            for (Item item : Items) {
+                ItemWriter.write('\n' + item.id + "" + item.category + "" + item.price + "" + "" + item.available_amount + "" + item.least_allowed_amount);
+            }
+        } catch (Exception e) {
             System.out.println("1");
         }
     }
