a376e9ea07a7db5d61415eb0dabf6cfec8148be4
The Hunter <nournoudy2006@gmail.com>
2025-12-28 14:30:43 +0300

    Fixed bugs in Item and Item Controller

--- src/main/java/com/mycompany/productionmanagmentsystem/Item.java
+++ src/main/java/com/mycompany/productionmanagmentsystem/Item.java
@@ -10,18 +10,32 @@ package com.mycompany.productionmanagmentsystem;
  */
 public class Item {
 
+    static int last_id = 0;
     int id;
     String category;
     int price;
     int available_amount;
     int least_allowed_amount;
 
-    Item(int id, String category, int price, int available_amount, int least_allowed_amount) {
+        Item(int id, String name, String category, int price, int available_amount, int least_allowed_amount) {
         this.id = id;
+        last_id = Math.max(id, last_id);
+        this.category = category;
+        this.price = price;
+        this.available_amount = available_amount;
+        this.least_allowed_amount = least_allowed_amount;}
+        
+        
+    Item(String category, String name, int price, int available_amount, int least_allowed_amount) {
+        this.id = last_id;
+        last_id++;
         this.category = category;
         this.price = price;
         this.available_amount = available_amount;
         this.least_allowed_amount = least_allowed_amount;
     }
+
+
+
 
 }
