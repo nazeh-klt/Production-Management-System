75f8791347a062498c3ec61f792a5a2be7e99351
Your Name <you@example.com>
2026-01-24 17:24:42 +0200

    tasks m

--- src/main/java/com/mycompany/productionmanagmentsystem/Tasks.java
+++ src/main/java/com/mycompany/productionmanagmentsystem/Tasks.java
@@ -4,6 +4,7 @@
  */
 package com.mycompany.productionmanagmentsystem;
 
+import javax.swing.*;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -13,7 +14,8 @@ import java.util.logging.Logger;
  *
  * @author samid
  */
-public class Tasks implements Runnable{
+public class Tasks implements Runnable {
+
     int ID;
     Product product;
     int requiredQuantity, achievedQuantity;
@@ -32,55 +34,55 @@ public class Tasks implements Runnable{
         this.deadlineDate = deadlineDate;
         this.status = status;
     }
-    
-    public double get_achieved_percentage(){
-        return (double)achievedQuantity/requiredQuantity*100;
+
+    public double get_achieved_percentage() {
+        return (double) achievedQuantity / requiredQuantity * 100;
     }
 
     @Override
     public void run() {
         int currentItemAmount;
         Scanner scan = new Scanner(System.in);
-        while(achievedQuantity < requiredQuantity){
-            for(Map.Entry<Item, Integer> i: product.required_items.entrySet()){
+        while (achievedQuantity < requiredQuantity) {
+            for (Map.Entry<Item, Integer> i : product.required_items.entrySet()) {
                 currentItemAmount = 0;
-                while(currentItemAmount < i.getValue()){
+                while (currentItemAmount < i.getValue()) {
                     i.getKey().available_amount--;
                     currentItemAmount++;
-                    if(i.getKey().available_amount == i.getKey().least_allowed_amount){
+                    if (i.getKey().available_amount == i.getKey().least_allowed_amount) {
                         System.out.println("WARNING!! The least threshold is being crossed");
                     }
-                    if(i.getKey().available_amount == 0){
+                    if (i.getKey().available_amount == 0) {
                         System.out.println(i.getKey().name + " current amount is " + i.getKey().available_amount);
                         break;
                     }
                 }
-                if(i.getKey().available_amount == 0){
-                    System.out.println(i.getKey().name + "amount is 0.");
-                    System.out.println("If you like to continue and abort the production of " + product.name +  " press 1");
-                    System.out.println("If you want to stop the process temporarly till items are available press 2");
-                    int n = scan.nextInt();
-                    if(n == 1){
-                        return;
-                    }
-                    else if(n ==  2){
-                        try {
-                            Thread.sleep(6000);
-                        } catch (InterruptedException ex) {
-                            System.out.println(ex.getMessage());
-                        }
-                    }
-                    else{
-                        try {
-                            throw new Exception("Please enter either 1 or 2");
-                        } catch (Exception ex) {
-                            System.out.println(ex.getMessage());
+                if (i.getKey().available_amount == 0) {
+                        Item item = i.getKey();
+                        String[] options = {"Abort Task", "Wait"};
+                        int choice = JOptionPane.showOptionDialog(
+                                null,
+                                "Item \"" + item.name + "\" is out of stock.\nWhat do you want to do?",
+                                "Out of Stock",
+                                JOptionPane.DEFAULT_OPTION,
+                                JOptionPane.WARNING_MESSAGE,
+                                null,
+                                options,
+                                options[0]
+                        );
+
+                        if (choice == 0) {
+                            return;
+                        } else if (choice == 1) {
+                            try {
+                                Thread.sleep(6000);
+                            } catch (InterruptedException ex) {
+                                System.out.println(ex.getMessage());
+                            }
                         }
                     }
                 }
-            }
             achievedQuantity++;
-            
         }
         scan.close();
     }
